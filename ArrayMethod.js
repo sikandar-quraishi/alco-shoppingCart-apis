7398be4a97473427ed37a046d3063817a6092010
sikandar-quraishi <squraishi8846@gmail.com>
2021-02-23 13:05:21 +0530

    some new methods

--- ArrayMethod.js
+++ ArrayMethod.js
@@ -6,6 +6,8 @@ fill(val,start,end) // Fill the elements in an array with a static value
 filter(function(currentval,index,arr),thisVal) //	Creates a new array with every element in an array that pass a test
 find(function(currentval,index,arr),thisVal) // Returns the value of the first element in an array that pass a test
 findIndex(function(currentval,index,arr),thisVal) // Returns the index of the first element in an array that pass a test
+flat()
+flatMap()
 forEach(function(currentval,index,arr),thisVal) // Calls a function for each array element
 from(obj,mapFunc,thisVal) // Creates an array from an object
 includes(element,start) // Check if an array contains the specified element
@@ -15,6 +17,7 @@ join(seperator) // Joins all elements of an array into a string
 keys() // Returns a Array Iteration Object, containing the keys of the original array
 lastIndexOf(element,start) // Search the array for an element, starting at the end, and returns its position
 map(function(currentval,index,arr),thisVal) // Creates a new array with the result of calling a function for each array element
+of()
 pop() // Removes the last element of an array, and returns that element
 push(item1,...) // Adds new elements to the end of an array, and returns the new length
 reduce(function(total,currentval,index,arr),initVal) // Reduce the values of an array to a single value (going left-to-right)
@@ -25,6 +28,8 @@ slice(start,end) // Selects a part of an array, and returns the new array
 some(function(currentval,index,arr),thisVal) // Checks if any of the elements in an array pass a test
 sort(compareFunc) // Sorts the elements of an array
 splice(index,quantity,item1,...) // Adds/Removes elements from an array
+toLocalString()
+toSource()
 toString() // Converts an array to a string, and returns the result
 unshift(item1,...) // Adds new elements to the beginning of an array, and returns the new length
-valueOf() // Returns the primitive value of an array
+values() // Returns the primitive value of an array
